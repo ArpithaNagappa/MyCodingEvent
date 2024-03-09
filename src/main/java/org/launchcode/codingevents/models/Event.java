8e903fe134277a739ac616a0bb7738d6bab0a061
Arpitha Nagappa <arpithagn021@gmail.com>
2024-03-09 12:56:12 -0600

    Committing

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -13,25 +13,6 @@ public class Event extends AbstractEntity{
     @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
-//    @Size(max = 500, message = "Description too long!")
-//    private String description;
-//    @NotBlank
-//    @Email(message = "Invalid email. Try again.")
-//    private String contactEmail;
-//    @NotNull(message="Location cannot be left blank.")
-//    @NotBlank(message="Location cannot be left blank.")
-//    private String location;
-//    @AssertTrue(message="This event must have attendee register.")
-//    private boolean shouldRegister = true;
-//    @NotNull(message="Number of attendees must be one or more.")
-//    @Min(value=1,message="Number of attendees must be one or more.")
-//    @Positive(message="Number of attendees must be one or more.")
-//    private int numberOfAttendees;
-//    @NotNull( message = "Must have number of food courses b/w 1 and 3")
-//    @Min(value = 1, message = "Must have number of food courses b/w 1 and 3")
-//    @Max(value = 3, message = "Must have number of food courses b/w 1 and 3")
-//    private int nmberOfFoodCourses;
-
     @OneToOne(cascade = CascadeType.ALL)
     @Valid
     @NotNull
@@ -82,40 +63,4 @@ public class Event extends AbstractEntity{
 
 }
 
-//    public int getNmberOfFoodCourses() {
-//        return nmberOfFoodCourses;
-//    }
-//
-//    public void setNmberOfFoodCourses(int nmberOfFoodCourses) {
-//        this.nmberOfFoodCourses = nmberOfFoodCourses;
-//    }
-
-//    public boolean isShouldRegister() {
-//        return shouldRegister;
-//    }
-//
-//    public int getNumberOfAttendees() {
-//        return numberOfAttendees;
-//    }
-//
-//    public void setNumberOfAttendees(int numberOfAttendees) {
-//        this.numberOfAttendees = numberOfAttendees;
-//    }
-//
-//
-//    public void setShouldRegister(boolean shouldRegister) {
-//        this.shouldRegister = shouldRegister;
-//    }
-//
-//    public String getLocation() {
-//        return location;
-//    }
-//
-//    public void setLocation(String location) {
-//        this.location = location;
-//    }
-
-//    public int getId() {
-//        return id;
-//    }
 
--- src/main/java/org/launchcode/codingevents/models/EventDetails.java
+++ src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -1,7 +1,6 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -15,8 +14,8 @@ public class EventDetails extends AbstractEntity {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    @OneToOne(mappedBy = "eventDetails")
-    private Event event;
+//    @OneToOne(mappedBy = "eventDetails")
+//    private Event event;
 
     public EventDetails(@Size(max = 500, message = "Description too long!") String description, @NotBlank(message = "Email is required") @Email(message = "Invalid email. Try again.") String contactEmail) {
         this.description = description;
